56c0d9a9b3e17e206671d295c59f54a8bbee620a
Dev <bd2010news@gmail.com>
2021-11-10 02:44:43 +0600

    java settings

--- app/src/main/java/com/appenjel/weatherdata/MainActivity.java
+++ app/src/main/java/com/appenjel/weatherdata/MainActivity.java
@@ -3,6 +3,9 @@ package com.appenjel.weatherdata;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.appenjel.sum.Sum;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Sum sum = new Sum();
+        Log.d("__data__",sum.d().toString());
 
     }
 }
